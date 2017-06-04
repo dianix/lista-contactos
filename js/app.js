ed928017c3c4e16dba4205b06b190af6c5073970
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-06-03 20:53:24 -0500

    validacion de datos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 var cargarPagina = function () {
     cantidadContactos();
-    var $datosContacto = $("#datosContacto");
-    //$datosContacto.submit(nuevoContacto);
+    //var $datosContacto = $("#datosContacto");
+    //$datosContacto.submit(nuevoContacto); <-- problemas con el submit
     var $enviarDatos = $("#enviarDatos");
     $enviarDatos.click(nuevoContacto);
-    
+
+    var $contacto = $("#contacto");
+    $contacto.keyup(validacionNombre);
+
+    //para aplicar la función borrarContacto a la tarjeta de ejemplo
     $("article button.close").click(borrarContacto);
 };
 
@@ -21,29 +25,54 @@ var cantidadContactos = function () {
 
 var nuevoContacto = function (e) {
     e.preventDefault;
+    //llamar elementos
     var $listaContactos = $("#listaContactos");
     var $nombreContacto = $("#contacto");
     var $telContacto = $("#telefono");
     var $enviarDatos = $("#enviarDatos");
-
+    // crear tarjeta nuevo cotacto
     var $tarjetaContacto = $("<article />").addClass("jumbotron");
-    var $tarjetaCerrar = $("<button/>").addClass('close glyphicon glyphicon-remove').attr('aria-label','Close');
+    var $tarjetaCerrar = $("<button/>").addClass('close glyphicon glyphicon-remove').attr('aria-label', 'Close');
     $tarjetaCerrar.click(borrarContacto);
     var $tarjetaNombre = $("<h4 />").text($nombreContacto.val());
-    console.log($nombreContacto.val())
     var $tarjetaTel = $("<p />").text($telContacto.val());
-
+    // agregar datos a tarjeta y tarjeta a lista
     $tarjetaContacto.append($tarjetaCerrar);
     $tarjetaContacto.append($tarjetaNombre);
     $tarjetaContacto.append($tarjetaTel);
     $listaContactos.append($tarjetaContacto);    
-    
+    limpiarDatos();    
+    // para actualizar el contador
     cantidadContactos();
 };
 
+var limpiarDatos = function(){
+    $("#contacto").val("");
+    $("#telefono").val("")
+}
+
 var borrarContacto = function () {
     $(this).parent().remove();
+    // para actualizar el contador
     cantidadContactos();
+};
+
+var validacionNombre = function () {
+    if ($(this).val().trim().length > 0 && $(this).val().trim().length < 20) {
+        var $telefono = $("#telefono");
+        $telefono.keyup(validacionTelefono);
+    }
+};
+
+var validacionTelefono = function () {
+    var $numero = $(this);
+    var $enviarDatos = $("#enviarDatos")
+    if ($numero.val().length === 10)
+        $enviarDatos.removeAttr("disabled");
+    else {
+        $enviarDatos.attr("disabled", true);
+    }
+
 };
 
 $(document).ready(cargarPagina);
